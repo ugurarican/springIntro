9c57b420f2d15f953e675719a9dab12871f0cbfc
Uğur ARICAN <ugur-arican@hotmail.com>
2023-01-18 15:25:42 +0300

    springIntro

---
+++ src/springIntro/CustomerManager.java
@@ -0,0 +1,21 @@
+package springIntro;
+
+public class CustomerManager implements ICustomerService{
+	private ICustomerDal customerDal;
+	
+
+	public void setCustomerDal(ICustomerDal customerDal) {
+		this.customerDal = customerDal;
+	}
+
+
+//	public CustomerManager(ICustomerDal customerDal) {
+//		this.customerDal = customerDal;	
+//	}
+
+
+	public void add() {
+		// iş kuralları
+		customerDal.add();
+	}
+}
